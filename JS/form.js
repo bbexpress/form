cba988b6cc44a8a6cd12e49e5532543bf3abe498
bbexpress <bbexpress@gmail.com>
2021-11-23 09:16:32 -0600

    design n beginning function

---
+++ JS/form.js
@@ -0,0 +1,17 @@
+
+function addUser(user){
+
+    console.log(user);
+
+    var table = document.getElementById("userForm");
+    console.log(table);
+
+    table.insertRow(0);
+    // cell.innerHTML = tableCount;
+    table.insertRow(1);
+    table.insertRow(2);
+    table.insertRow(3);
+
+    // cell.innerHTML = tableCount;
+}
+    
